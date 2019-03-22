28f2e969921a2ae0506c8790908e916bf96d1f3b
makutak <takkouno@gmail.com>
2019-03-22 21:46:40 +0900

    replace double quote

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -5,8 +5,8 @@ export async function notify(message: string) {
     const url = "https://notify-api.line.me/api/notify"
     const token = process.env.TOKEN;
     const headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': `Bearer ${token}`
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": `Bearer ${token}`,
     };
     const params = querystring.stringify({
         message
